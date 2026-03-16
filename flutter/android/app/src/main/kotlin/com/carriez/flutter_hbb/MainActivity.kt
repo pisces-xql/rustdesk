05737364a75f593567560bf599cfe37673c7d91d
pisces-xql <pisces-xql@users.noreply.github.com>
2026-03-16 14:20:21 +0800

    save id

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainActivity.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainActivity.kt
@@ -33,6 +33,7 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import kotlin.concurrent.thread
+import java.io.File
 
 
 class MainActivity : FlutterActivity() {
